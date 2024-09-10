6f8bcf33956f8f4ebd91d05bc8923da817f09318
Jeferson Souza <jeferson.2017317264@gmail.com>
2024-09-10 01:38:56 -0300

    Incident creation

--- frontend/src/containers/incident/components/NewIncidentLocationPicker.tsx
+++ frontend/src/containers/incident/components/NewIncidentLocationPicker.tsx
@@ -1,5 +1,5 @@
 import { LatLng, LatLngExpression, LeafletMouseEvent } from "leaflet";
-import { useEffect, useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { MapContainer, useMapEvent, TileLayer, Marker } from "react-leaflet";
 import { Skeleton } from "../../../components/ui/Skeleton";
 import { useNewIncident } from "../hooks/useNewIncident";
@@ -26,30 +26,22 @@ function LocationMarker({ defaultValue, onChange }: LocationMarkerProps) {
 }
 
 export function NewIncidentLocationPicker() {
-  const { handleChangeValue } = useNewIncident();
-  const [initialPosition, setInitialPosition] =
-    useState<LatLngExpression | null>(null);
+  const { data, handleChangeValue } = useNewIncident();
+  const initialPosition: LatLngExpression | null = useMemo(
+    () =>
+      data.latitude && data.longitude
+        ? [Number(data.latitude), Number(data.longitude)]
+        : null,
+    [data.latitude, data.longitude]
+  );
 
   const handleSelectPosition = useCallback(
     (value: LatLng) => {
-      handleChangeValue(value.lat, "latitude");
-      handleChangeValue(value.lng, "longitude");
+      handleChangeValue(String(value.lat), "latitude");
+      handleChangeValue(String(value.lng), "longitude");
     },
     [handleChangeValue]
   );
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setInitialPosition([latitude, longitude]);
-      },
-      (error) => {
-        console.error("Geolocation error:", error);
-        setInitialPosition([-27.0127149, -51.1714452]); // Example coordinates (Videira)
-      }
-    );
-  }, []);
 
   if (initialPosition === null) {
     return <Skeleton className="h-[400px] rounded-md" />;
@@ -57,7 +49,7 @@ export function NewIncidentLocationPicker() {
 
   return (
     <div className="h-[400px] rounded-md overflow-hidden">
-      <MapContainer scrollWheelZoom={false} center={initialPosition} zoom={15}>
+      <MapContainer center={initialPosition} zoom={15}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
